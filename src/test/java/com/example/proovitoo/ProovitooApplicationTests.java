cfefb79a764592735e73177365de8e1b2aa21517
Margus Apinis <maapin@taltech.ee>
2026-02-27 13:18:29 +0200

    Initial Spring boot + web

---
+++ src/test/java/com/example/proovitoo/ProovitooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proovitoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProovitooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
