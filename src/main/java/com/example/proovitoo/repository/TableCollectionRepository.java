8e089f96420fac2785527bf79a3ee976ccf82d19
Margus Apinis <maapin@taltech.ee>
2026-02-28 19:50:13 +0200

    vist vale tee...

--- src/main/java/com/example/proovitoo/repository/TableCollectionRepository.java
+++ src/main/java/com/example/proovitoo/repository/TableCollectionRepository.java
@@ -1,9 +1,13 @@
 package com.example.proovitoo.repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Repository;
+import org.yaml.snakeyaml.Yaml;
+import java.io.InputStream;
+
 import com.example.proovitoo.model.Table;
 import com.example.proovitoo.model.Tags;
 import com.example.proovitoo.model.Status;
@@ -13,12 +17,24 @@ public class TableCollectionRepository {
     private final List<Table> tables = new ArrayList<>();
 
     public TableCollectionRepository(){
-        tables.add(new Table(1, 4, Status.AVAILABLE, Tags.NEAR_WINDOW));
-        tables.add(new Table(2, 2, Status.AVAILABLE, Tags.QUIET_AREA));
-        tables.add(new Table(3, 6, Status.RESERVED, Tags.CHILD_FRIENDLY));
-        tables.add(new Table(4, 4, Status.AVAILABLE, Tags.OUTDOOR_SEATING));
-        tables.add(new Table(5, 8, Status.RESERVED, Tags.PRIVATE_ROOM));
-        tables.add(new Table(6, 2, Status.RESERVED, Tags.OUTDOOR_SEATING));
+        
+        Yaml yaml = new Yaml();
+        InputStream inputStream = this.getClass()
+        .getClassLoader()
+        .getResourceAsStream("tables.yaml");
+        List<Table> obj = yaml.load(inputStream);
+        //System.out.println(obj);
+        //obj.forEach(table -> System.out.println(table.getClass().toString()));
+        //System.out.println(yaml.loadAll(inputStream).getClass().toString());
+        //tables.addAll(obj);
+        //*/
+        /*
+        tables.add(new Table(1, 4, Status.AVAILABLE, new Tags[]{Tags.NEAR_WINDOW, Tags.CHILD_FRIENDLY}));
+        tables.add(new Table(2, 2, Status.AVAILABLE, new Tags[]{Tags.QUIET_AREA}));
+        tables.add(new Table(3, 6, Status.RESERVED, new Tags[]{Tags.CHILD_FRIENDLY}));
+        tables.add(new Table(4, 4, Status.AVAILABLE, new Tags[]{Tags.OUTDOOR_SEATING}));
+        tables.add(new Table(5, 8, Status.RESERVED, new Tags[]{Tags.PRIVATE_ROOM}));
+        tables.add(new Table(6, 2, Status.RESERVED, new Tags[]{Tags.OUTDOOR_SEATING})); //*/
     }
 
     public List<Table> getAllTables() {
