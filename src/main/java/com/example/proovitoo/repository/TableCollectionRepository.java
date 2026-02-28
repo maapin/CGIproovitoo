c68afce935808b218a1b80bf72dff37a728e422d
Margus Apinis <maapin@taltech.ee>
2026-02-28 23:07:56 +0200

    loeme laudade confi hoopis yaml failist

--- src/main/java/com/example/proovitoo/repository/TableCollectionRepository.java
+++ src/main/java/com/example/proovitoo/repository/TableCollectionRepository.java
@@ -1,27 +1,40 @@
 package com.example.proovitoo.repository;
 
 import java.util.List;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.springframework.asm.TypeReference;
 import org.springframework.stereotype.Repository;
 import com.example.proovitoo.model.Table;
 import com.example.proovitoo.model.Tags;
+
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.dataformat.yaml.YAMLFactory;
+
 import com.example.proovitoo.model.Status;
 
 @Repository
 public class TableCollectionRepository {
     private final List<Table> tables = new ArrayList<>();
+    private final ObjectMapper objectMapper;  
+
 
     public TableCollectionRepository(){
 
-        /*
-        tables.add(new Table(1, 4, Status.AVAILABLE, new Tags[]{Tags.NEAR_WINDOW}));
-        tables.add(new Table(2, 2, Status.AVAILABLE, new Tags[]{Tags.QUIET_AREA}));
-        tables.add(new Table(3, 6, Status.RESERVED, new Tags[]{Tags.CHILD_FRIENDLY}));
-        tables.add(new Table(4, 4, Status.AVAILABLE, new Tags[]{Tags.OUTDOOR_SEATING}));
-        tables.add(new Table(5, 8, Status.RESERVED, new Tags[]{Tags.PRIVATE_ROOM}));
-        tables.add(new Table(6, 2, Status.RESERVED, new Tags[]{Tags.OUTDOOR_SEATING}));
-        // */
+        this.objectMapper = new ObjectMapper(new YAMLFactory());
+
+        try {
+           InputStream yamlInputStream = TypeReference.class.getResourceAsStream("/tables.yaml");
+           Table[] loadedTables = this.objectMapper.readValue(yamlInputStream, Table[].class);
+           for (Table table : loadedTables) {
+                System.out.println("Loaded table: " + table);
+                tables.add(table);
+           }
+        } catch (Exception e) {
+            System.err.println("Error reading tables.yaml: " + e.getMessage());
+        }
+
     }
 
     public List<Table> getAllTables() {
